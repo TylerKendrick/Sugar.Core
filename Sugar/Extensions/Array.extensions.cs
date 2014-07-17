95d0f8fe4935c3c37f3c88649d0530e054983008
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-07-17 16:16:43 -0500

    Added array extensions Moved executionblock

---
+++ Sugar/Extensions/Array.extensions.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace System
+{
+    public static class ArrayExtensions
+    {
+        public static void Clear(this Array self, int index, int length)
+        {
+            Array.Clear(self, index, length);
+        }
+        public static void Clear(this Array self, int index)
+        {
+            Array.Clear(self, index, self.Length);
+        }
+        public static void Clear(this Array self)
+        {
+            Array.Clear(self, 0, self.Length);
+        }
+
+        public static int BinarySearch(this Array self, int index, int length, object value, IComparer comparer)
+        {
+            return Array.BinarySearch(self, index, length, value, comparer);
+        }
+        public static int BinarySearch(this Array self, int index, int length, object value)
+        {
+            return Array.BinarySearch(self, index, length, value);
+        }
+        public static int BinarySearch(this Array self, int index, object value)
+        {
+            return Array.BinarySearch(self, index, self.Length, value);
+        }
+        public static int BinarySearch(this Array self, object value)
+        {
+            return Array.BinarySearch(self, value);
+        }
+
+
+        public static int BinarySearch<T>(this T[] self, int index, int length, T value, IComparer<T> comparer)
+        {
+            return Array.BinarySearch(self, index, length, value, comparer);
+        }
+        public static int BinarySearch<T>(this T[] self, int index, int length, T value)
+        {
+            return Array.BinarySearch(self, index, length, value);
+        }
+        public static int BinarySearch<T>(this T[] self, int index, T value)
+        {
+            return Array.BinarySearch(self, index, self.Length, value);
+        }
+        public static int BinarySearch<T>(this T[] self, T value)
+        {
+            return Array.BinarySearch(self, value);
+        }
+        
+        public static void Sort(this Array self, int index, int length, IComparer comparer)
+        {
+            Array.Sort(self, index, length, comparer);
+        }
+        public static void Sort(this Array self, int index, int length)
+        {
+            Array.Sort(self, index, length);
+        }
+        public static void Sort(this Array self, int index)
+        {
+            Array.Sort(self, index, self.Length);
+        }
+        public static void Sort(this Array self)
+        {
+            Array.Sort(self);
+        }
+
+
+        public static void Sort<T>(this T[] self, int index, int length, IComparer<T> comparer)
+        {
+            Array.Sort(self, index, length, comparer);
+        }
+        public static void Sort<T>(this T[] self, int index, int length)
+        {
+            Array.Sort(self, index, length);
+        }
+        public static void Sort<T>(this T[] self, int index)
+        {
+            Array.Sort(self, index, self.Length);
+        }
+        public static void Sort<T>(this T[] self)
+        {
+            Array.Sort(self);
+        }
+
+        public static void ForEach<T>(this T[] self, Action<T> action)
+        {
+            Array.ForEach(self, action);
+        }
+
+        public static ReadOnlyCollection<T> AsReadOnly<T>(this T[] self)
+        {
+            return Array.AsReadOnly(self);
+        }
+    }
+}
