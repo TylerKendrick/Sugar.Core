50b2af63018068a76b55bd8a05a2035bde7ba295
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-07-26 13:52:51 -0500

    Operators.Byte: - Added XML Doc comments - Reduced Cyclomatic Complexity - Improved Maintainability Index

---
+++ Sugar/Operations.Byte.cs
@@ -0,0 +1,75 @@
+﻿internal static partial class Operations
+{
+    internal static class Byte
+    {
+        internal static int OnShiftLeft(byte left, int right)
+        {
+            return left << right;
+        }
+
+
+        internal static int OnShiftRight(byte left, int right)
+        {
+            return left >> right;
+        }
+
+
+        internal static int OnAnd(byte left, byte right)
+        {
+            return left & right;
+        }
+
+
+        internal static int OnOr(byte left, byte right)
+        {
+            return left | right;
+        }
+
+
+        internal static int OnXOr(byte left, byte right)
+        {
+            return left ^ right;
+        }
+
+        internal static bool OnGreaterThan(byte left, byte right)
+        {
+            return left > right;
+        }
+
+
+        internal static bool OnGreaterThanOrEqual(byte left, byte right)
+        {
+            return left >= right;
+        }
+
+
+        internal static bool OnLessThan(byte left, byte right)
+        {
+            return left < right;
+        }
+
+
+        internal static bool OnLessThanOrEqual(byte left, byte right)
+        {
+            return left <= right;
+        }
+
+
+        internal static bool OnEquality(byte left, byte right)
+        {
+            return left == right;
+        }
+
+
+        internal static bool OnInequality(byte left, byte right)
+        {
+            return left != right;
+        }
+
+
+        internal static int OnNot(byte target)
+        {
+            return ~target;
+        }
+    }
+}
--- Sugar/Operators.Byte.cs
+++ Sugar/Operators.Byte.cs
@@ -6,18 +6,72 @@
     /// </summary>
     public static class Byte
     {
-        public static readonly Binary<byte, int, int> ShiftLeft = (left, right) => left << right;
-        public static readonly Binary<byte, int, int> ShiftRight = (left, right) => left >> right;
-        public static readonly Binary<byte, byte, int> And = (left, right) => left & right;
-        public static readonly Binary<byte, byte, int> Or = (left, right) => left | right;
-        public static readonly Binary<byte, byte, int> XOr = (left, right) => left ^ right;
-        public static readonly Unary<byte, int> Not = target => ~target;
+        #region Bitwise Operators
 
-        public static readonly Binary<byte, byte, bool> GreaterThan = (left, right) => left > right;
-        public static readonly Binary<byte, byte, bool> GreaterThanOrEqual = (left, right) => left >= right;
-        public static readonly Binary<byte, byte, bool> LessThan = (left, right) => left < right;
-        public static readonly Binary<byte, byte, bool> LessThanOrEqual = (left, right) => left <= right;
-        public static readonly Binary<byte, byte, bool> Equality = (left, right) => left == right;
-        public static readonly Binary<byte, byte, bool> Inequality = (left, right) => left != right;
+        /// <summary>
+        /// Exposes the left shift operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, int, int> ShiftLeft = Operations.Byte.OnShiftLeft;
+
+        /// <summary>
+        /// Exposes the left shift operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, int, int> ShiftRight = Operations.Byte.OnShiftRight;
+
+        /// <summary>
+        /// Exposes the bitwise and operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, int> And = Operations.Byte.OnAnd;
+
+        /// <summary>
+        /// Exposes the bitwise inclusive-or operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, int> Or = Operations.Byte.OnOr;
+
+        /// <summary>
+        /// Exposes the bitwise exclusive-or operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, int> XOr = Operations.Byte.OnXOr;
+
+        /// <summary>
+        /// Exposes the bitwise Not operator of Byte as a delegate
+        /// </summary>
+        public static readonly Unary<byte, int> Not = Operations.Byte.OnNot;
+
+        #endregion Bitwise Operators
+
+        #region Comparison Operators
+
+        /// <summary>
+        /// Exposes the Comparison Greater-Than operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> GreaterThan = Operations.Byte.OnGreaterThan;
+
+        /// <summary>
+        /// Exposes the Comparison Greater-Than-Or-Equal operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> GreaterThanOrEqual = Operations.Byte.OnGreaterThanOrEqual;
+
+        /// <summary>
+        /// Exposes the Comparison Less-Than operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> LessThan = Operations.Byte.OnLessThan;
+
+        /// <summary>
+        /// Exposes the Comparison Less-Than-Or-Equal operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> LessThanOrEqual = Operations.Byte.OnLessThanOrEqual;
+
+        /// <summary>
+        /// Exposes the Comparison Equality operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> Equality = Operations.Byte.OnEquality;
+
+        /// <summary>
+        /// Exposes the Comparison Inequality operator of Byte as a delegate
+        /// </summary>
+        public static readonly Binary<byte, byte, bool> Inequality = Operations.Byte.OnInequality;
+        
+        #endregion Comparison Operators
     }
 }
