1f991329c89b57d89ff4dc4f568868cab40d176e
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-07-29 20:11:07 -0500

    Added generic event args class and initial tests.

---
+++ Sugar.Tests/UnitTestOfEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sugar.Tests
+{
+    class UnitTestOfEventArgs : UnitTestOf<EventArgs<int>>
+    {
+        private const int Expectation = 4;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        protected override void SetUpMocks()
+        {
+        }
+
+        protected override EventArgs<int> SetUpConcern()
+        {
+            return EventArgs<int>.Create(Expectation);
+        }
+
+        [Test]
+        public void WiringTest()
+        {
+            var result = Concern.Value;
+
+            Assert.That(Concern, Is.InstanceOf<EventArgs>());
+            Assert.That(result, Is.EqualTo(Expectation));
+        }
+    }
+}
---
+++ Sugar/EventArgs.cs
@@ -0,0 +1,23 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Contains event data of a specified type <typeparamref name="T"/>.
+    /// </summary>
+    public class EventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Exposes the constructor as a delegate.
+        /// </summary>
+        public static readonly Func<T, EventArgs<T>> Create = value => new EventArgs<T>(value);
+        
+        /// <summary>
+        /// Exposes the wrapped value for the specified event data type.
+        /// </summary>
+        public T Value { get; private set; }
+
+        private EventArgs(T value)
+        {
+            Value = value;
+        }
+    }
+}
