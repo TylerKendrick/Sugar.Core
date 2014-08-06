a0bd0c18f552a95e1ecce173a9b733ae3174992d
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-08-05 22:14:06 -0500

    Added tests for require.that

---
+++ Sugar.Tests/StaticRequireTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sugar.Tests
+{
+    [TestFixture, Category("Unit Test")]
+    public class StaticRequireTests
+    {
+        private readonly object _concern = new object();
+
+        [Test]
+        public void ThatMethodThrowsExceptionWithFalseCondition()
+        {
+            Assert.That(() => Require.That(false),
+                Throws.InvalidOperationException);
+        }
+
+
+        [Test]
+        public void ThatMethodDoesNotThrowExceptionWithTrueCondition()
+        {
+            Assert.That(() => Require.That(true),
+                Throws.Nothing);
+        }
+
+
+        [Test]
+        public void ThatMethodThrowsExceptionWithFalsePredicate()
+        {
+            Assert.That(() => Require.That(_concern, o => false),
+                Throws.InvalidOperationException);
+        }
+
+
+        [Test]
+        public void ThatMethodDoesNotThrowExceptionWithTruePredicate()
+        {
+            Assert.That(() => Require.That(_concern, o => true),
+                Throws.Nothing);
+        }
+    }
+}
